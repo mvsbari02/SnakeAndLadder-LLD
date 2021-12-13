4c8756fdbd41417e48924c17356aa2049744635a
mvsbari02 <72955116+mvsbari02@users.noreply.github.com>
2021-12-13 17:00:38 +0530

    Update Board.java

--- Snake and Ladders/Board.java
+++ Snake and Ladders/Board.java
@@ -9,81 +9,6 @@ public interface Board {
   public int getDestination();
 }
 
-class Board1 {
-  private int destination = 100;
-  private HashMap<Integer,Snake> snakes = new  HashMap<Integer,Snake>();
-  private HashMap<Integer,Ladder> ladders = new  HashMap<Integer,Ladder>();
-  public int getDestination() {
-    return destination;
-  }
-  public void setSnakesAndLadders() {
-    for (int i = 10,d = 6;i < destination;i = 2*i - 6,d++) {
-      Snake snake = new Snake (i,i-d);
-      snakes.put(i,snake);
-    }
-    for (int i = 6,u = 11;i<destination-15;i = 2*i - 4,u = 2*u + 1) {
-      Ladder ladder = new Ladder (i,u);
-      ladders.put(i,ladder);
-    }
-  }
-  public HashMap<Integer,Snake> getSnakes() {
-    return snakes;
-  }
-  public HashMap<Integer,Ladder> getLadders() {
-    return ladders;
-  }
-}
-
-class Board2 {
-  private int destination = 100;
-  private HashMap<Integer,Snake> snakes = new  HashMap<Integer,Snake>();
-  private HashMap<Integer,Ladder> ladders = new  HashMap<Integer,Ladder>();
-  public int getDestination() {
-    return destination;
-  }
-  public void setSnakesAndLadders() {
-    for (int i = 10,d = 6;i < destination;i = 2*i - 6,d++) {
-      Snake snake = new Snake (i,i-d);
-      snakes.put(i,snake);
-    }
-    for (int i = 6,u = 11;i<destination-15;i = 2*i - 4,u = 2*u + 1) {
-      Ladder ladder = new Ladder (i,u);
-      ladders.put(i,ladder);
-    }
-  }
-  public HashMap<Integer,Snake> getSnakes() {
-    return snakes;
-  }
-  public HashMap<Integer,Ladder> getLadders() {
-    return ladders;
-  }
-}
-
-class Board3 {
-  private int destination = 100;
-  private HashMap<Integer,Snake> snakes = new  HashMap<Integer,Snake>();
-  private HashMap<Integer,Ladder> ladders = new  HashMap<Integer,Ladder>();
-  public int getDestination() {
-    return destination;
-  }
-  public void setSnakesAndLadders() {
-    for (int i = 10,d = 6;i < destination;i = 2*i - 6,d++) {
-      Snake snake = new Snake (i,i-d);
-      snakes.put(i,snake);
-    }
-    for (int i = 6,u = 11;i<destination-15;i = 2*i - 4,u = 2*u + 1) {
-      Ladder ladder = new Ladder (i,u);
-      ladders.put(i,ladder);
-    }
-  }
-  public HashMap<Integer,Snake> getSnakes() {
-    return snakes;
-  }
-  public HashMap<Integer,Ladder> getLadders() {
-    return ladders;
-  }
-}
-
 class UserBoard implements Board{
   private int destination = 100;
   private HashMap<Integer,Snake> snakes = new  HashMap<Integer,Snake>();
@@ -105,3 +30,81 @@ class UserBoard implements Board{
     return ladders;
   }
 }
+
+/* Custom made boards with predefined positions for snakes and ladders*/ 
+
+class CustomBoard1 implements Board{
+  private int destination = 100;
+  private HashMap<Integer,Snake> snakes = new  HashMap<Integer,Snake>();
+  private HashMap<Integer,Ladder> ladders = new  HashMap<Integer,Ladder>();
+  public int getDestination() {
+    return destination;
+  }
+  public void setSnakesAndLadders() {
+    for (int i = 10,d = 6;i < destination;i = 2*i - 6,d++) {
+      Snake snake = new Snake (i,i-d);
+      snakes.put(i,snake);
+    }
+    for (int i = 6,u = 11;i<destination-15;i = 2*i - 4,u = 2*u + 1) {
+      Ladder ladder = new Ladder (i,u);
+      ladders.put(i,ladder);
+    }
+  }
+  public HashMap<Integer,Snake> getSnakes() {
+    return snakes;
+  }
+  public HashMap<Integer,Ladder> getLadders() {
+    return ladders;
+  }
+}
+/*
+class CustomBoard2 implements Board{
+  private int destination = 100;
+  private HashMap<Integer,Snake> snakes = new  HashMap<Integer,Snake>();
+  private HashMap<Integer,Ladder> ladders = new  HashMap<Integer,Ladder>();
+  public int getDestination() {
+    return destination;
+  }
+  public void setSnakesAndLadders() {
+    for (int i = 10,d = 6;i < destination;i = 2*i - 6,d++) {
+      Snake snake = new Snake (i,i-d);
+      snakes.put(i,snake);
+    }
+    for (int i = 6,u = 11;i<destination-15;i = 2*i - 4,u = 2*u + 1) {
+      Ladder ladder = new Ladder (i,u);
+      ladders.put(i,ladder);
+    }
+  }
+  public HashMap<Integer,Snake> getSnakes() {
+    return snakes;
+  }
+  public HashMap<Integer,Ladder> getLadders() {
+    return ladders;
+  }
+}
+
+class CustomBoard3 implements Board {
+  private int destination = 100;
+  private HashMap<Integer,Snake> snakes = new  HashMap<Integer,Snake>();
+  private HashMap<Integer,Ladder> ladders = new  HashMap<Integer,Ladder>();
+  public int getDestination() {
+    return destination;
+  }
+  public void setSnakesAndLadders() {
+    for (int i = 10,d = 6;i < destination;i = 2*i - 6,d++) {
+      Snake snake = new Snake (i,i-d);
+      snakes.put(i,snake);
+    }
+    for (int i = 6,u = 11;i<destination-15;i = 2*i - 4,u = 2*u + 1) {
+      Ladder ladder = new Ladder (i,u);
+      ladders.put(i,ladder);
+    }
+  }
+  public HashMap<Integer,Snake> getSnakes() {
+    return snakes;
+  }
+  public HashMap<Integer,Ladder> getLadders() {
+    return ladders;
+  }
+}
+*/
